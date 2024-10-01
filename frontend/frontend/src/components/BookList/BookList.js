0c6317eb3cb7b69d31931f3b48e7a3db7229d572
Andrej2422 <cargocult1986@gmail.com>
2024-10-01 12:24:36 +0200

    Filter Redux slice and filter by title

--- frontend/frontend/src/components/BookList/BookList.js
+++ frontend/frontend/src/components/BookList/BookList.js
@@ -1,13 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs'
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators';
+import { selectTitleFilter } from '../../redux/slices/filterSlice';
 import './BookList.css'
 
 
 
 const BookList = () => {
     const books = useSelector((state) => state.books)
-
+    const titleFilter = useSelector(selectTitleFilter)
     const dispatch = useDispatch()
 
     const handleDeleteBook = (id) => {
@@ -18,6 +19,13 @@ const BookList = () => {
         dispatch(toggleFavorite(id))
     }
 
+    const filteredBooks = books.filter((book) => {
+        const matchesTitle = book.title
+            .toLowerCase()
+            .includes(titleFilter.toLowerCase())
+        return matchesTitle
+    })
+
     return (
         <div className="app-block book-list">
             <h2>Book List</h2>
@@ -25,13 +33,16 @@ const BookList = () => {
                 <p>No books available</p>
             ) : (
                 <ul>
-                    {books.map((book, index) => (
+                    {filteredBooks.map((book, index) => (
                         <li key={book.id}>
                             <div className="book-info">
-                                {++index}. {book.title} by <strong>{book.author}</strong>
+                                {++index}. {book.title} by{''}
+                                <strong>{book.author}</strong>
                             </div>
                             <div className="book-actions">
-                                <span onClick={() => handleToggleFavorite(book.id)}>
+                                <span
+                                    onClick={() =>
+                                        handleToggleFavorite(book.id)}>
                                     {book.isFavorite ? (
                                         <BsBookmarkStarFill className="star-icon" />
                                     ) : (
