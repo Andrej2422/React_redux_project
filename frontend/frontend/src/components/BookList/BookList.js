9115bd6a2a65e6332597864857714d6c8c6d36f6
Andrej2422 <cargocult1986@gmail.com>
2024-10-02 12:32:31 +0200

    Highlight filter text matches

--- frontend/frontend/src/components/BookList/BookList.js
+++ frontend/frontend/src/components/BookList/BookList.js
@@ -36,6 +36,22 @@ const BookList = () => {
         return matchesTitle && matchesAuthor && matchesFavorite;
     })
 
+    const highlightMatch = (text, filter) => {
+        if (!filter) return text;
+
+        const regex = new RegExp(`(${filter})`, 'gi');
+        return text.split(regex).map((substring, index) => {
+            if (substring.toLowerCase() === filter.toLowerCase()) {
+                return (
+                    <span key={index} className="highlight">
+                        {substring}
+                    </span>
+                )
+            }
+            return substring
+        })
+    };
+
     return (
         <div className="app-block book-list">
             <h2>Book List</h2>
@@ -46,20 +62,23 @@ const BookList = () => {
                     {filteredBooks.map((book, index) => (
                         <li key={book.id}>
                             <div className="book-info">
-                                {++index}. {book.title} by{''}
-                                <strong>{book.author}</strong>
+                                {++index}. {highlightMatch(book.title, titleFilter)} by{' '}
+                                <strong>{highlightMatch(book.author, authorFilter)}</strong>
                             </div>
                             <div className="book-actions">
                                 <span
                                     onClick={() =>
-                                        handleToggleFavorite(book.id)}>
+                                        handleToggleFavorite(book.id)
+                                    }
+                                >
                                     {book.isFavorite ? (
                                         <BsBookmarkStarFill className="star-icon" />
                                     ) : (
                                         <BsBookmarkStar className="star-icon" />
                                     )}
                                 </span>
-                                <button onClick={() => handleDeleteBook(book.id)}>
+                                <button onClick={() => handleDeleteBook(book.id)}
+                                >
                                     Delete
                                 </button>
                             </div>
