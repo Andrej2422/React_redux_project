ee655755827a73059b287edca1a75c74349988a7
Andrej2422 <cargocult1986@gmail.com>
2024-10-04 15:42:44 +0200

    Dispatch error in the async thunk

--- frontend/frontend/src/redux/slices/booksSlice.js
+++ frontend/frontend/src/redux/slices/booksSlice.js
@@ -1,14 +1,20 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import createBookWithId from '../../utils/createBookWithId'
+import { setError } from './errorSlice'
 
 const initialState = []
 
 export const fetchBook = createAsyncThunk(
     'books/fetchBook',
-    async () => {
-        const res = await axios.get('http://localhost:4000/random-book')
-        return res.data
+    async (url, thunkAPI) => {
+        try {
+            const res = await axios.get(url)
+            return res.data
+        } catch (error) {
+            thunkAPI.dispatch(setError(error.message))
+            throw error
+        }
     }
 )
 
@@ -40,7 +46,7 @@ const booksSlice = createSlice({
             if (action.payload.title && action.payload.author) {
                 state.push(createBookWithId(action.payload, 'API'))
             }
-        })
+        });
     },
 })
 
